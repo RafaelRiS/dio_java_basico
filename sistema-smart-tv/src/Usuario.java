32bb1a5762cf182a9dcd74fd49a9f77c67f683a0
Rafael <ralkribeiro@hotmail.com>
2024-06-25 18:06:10 -0300

    Atualizacao 25/06/2024

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,25 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+            SmartTv smartTv = new SmartTv();
+
+            smartTv.ligar ();
+            System.out.println("TV Ligada? " + smartTv.ligada);
+
+            smartTv.diminuirVolume();
+            smartTv.diminuirVolume();
+            smartTv.diminuirVolume();
+            smartTv.aumentarVolume();
+            System.out.println("Volume Atual: " + smartTv.volume);
+
+            System.out.println("Canal Atual: " + smartTv.canal);
+            smartTv.mudarCanal(13);
+            System.out.println("Canal Atual: " + smartTv.canal);
+
+            System.out.println("Volume Atual: " + smartTv.volume);
+
+            smartTv.desligar();
+            System.out.println("Novo status -> TV Ligada? " + smartTv.ligada);
+    }       
+
+}
